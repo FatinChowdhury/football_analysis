638dabb08f9c7ef28c8e3cb08351a9f647147dc4
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2024-06-11 23:59:34 -0400

    Update yolo_inference.py

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
-
+print("hello")
 # inference for object
-model = YOLO('yolov8l')
+model = YOLO('models/best.pt')
 results = model.predict('input_videos/08fd33_4.mp4',save=True)
 print(results[0])
 print('=================================')
